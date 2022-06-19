54077f3b40917942f3331897159019e74bc8016a
sanjaysojan-dev <sanjaysojan@gmail.com>
2022-06-19 12:25:28 +0100

    Added email input field

--- lib/screens/ForgotPasswordScreen.dart
+++ lib/screens/ForgotPasswordScreen.dart
@@ -1,47 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
 class ForgotPasswordScreen extends StatefulWidget {
-
-  const ForgotPasswordScreen ({Key? key}) : super (key: key);
+  const ForgotPasswordScreen({Key? key}) : super(key: key);
 
   @override
   _ForgotPasswordState createState() => _ForgotPasswordState();
-
 }
 
-class _ForgotPasswordState extends State <ForgotPasswordScreen> {
-
+class _ForgotPasswordState extends State<ForgotPasswordScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final email = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Please enter your email ";
+        }
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
 
-  
-
-
-
-
-
-    return Scaffold (
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: IconButton (
-          icon: Icon(Icons.arrow_back, color: Colors.blueAccent,),
-          onPressed: (){
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.blueAccent,
+          ),
+          onPressed: () {
             Navigator.of(context).pop();
           },
         ),
       ),
-      body:  Stack (
-        children:[ SingleChildScrollView (
-          child: Container (
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Container(
             color: Colors.white,
-
             child: Padding(
               padding: const EdgeInsets.fromLTRB(36, 0, 36, 10),
               child: Form(
@@ -54,34 +63,32 @@ class _ForgotPasswordState extends State <ForgotPasswordScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: const [
-                        Text("RESET YOUR PASSWORD",
-                        style: TextStyle(
-                          color: Colors.black45,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15
-                        ),)
+                        Text(
+                          "RESET YOUR PASSWORD",
+                          style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15),
+                        )
                       ],
                     ),
                     SizedBox(height: 15),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: const [
-                        Flexible(child:
-                        Text("Type in your email address and "
-                            "we'll send you an email to create a new password.",
+                        Flexible(
+                            child: Text(
+                          "Type in your email address and "
+                          "we'll send you an email to create a new password.",
                           style: TextStyle(
                               color: Colors.blueAccent,
                               fontWeight: FontWeight.bold,
-                              fontSize: 15
-                          ),
-                        )
-                        )
+                              fontSize: 15),
+                        ))
                       ],
                     ),
-
-                    SizedBox(height: 45),
-
-
+                    SizedBox(height: 15),
+                    email
                   ],
                 ),
               ),
@@ -89,8 +96,6 @@ class _ForgotPasswordState extends State <ForgotPasswordScreen> {
           ),
         ),
       ]),
-
     );
   }
-
 }
