3953cc1c522ad902f53d5a8a010f2c34fd3f8268
sanjaysojan-dev <sanjaysojan@gmail.com>
2022-06-19 11:30:18 +0100

    Added forgot password screen

---
+++ lib/screens/ForgotPasswordScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+
+  const ForgotPasswordScreen ({Key? key}) : super (key: key);
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+
+}
+
+class _ForgotPasswordState extends State <ForgotPasswordScreen> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
