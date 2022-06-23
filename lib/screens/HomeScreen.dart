97bc0b468dda2ac20605b54ba28657c411b97426
sanjaysojan-dev <sanjaysojan@gmail.com>
2022-06-23 19:29:22 +0100

    Added popup menu to sign out of app

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -13,77 +13,108 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final _googleSignIn = GoogleSignIn();
-  var  userName = Text("");
-  var  userEmail = Text("");
+  var userName = Text("");
+  var userEmail = Text("");
 
   User? user = FirebaseAuth.instance.currentUser;
   UserModel userModel = UserModel();
 
-
-
   @override
-  void initState () {
+  void initState() {
     super.initState();
-
 
     FirebaseFirestore.instance
         .collection("users")
-        .doc(user!.uid).get()
-        .then((value){
-          this.userModel = UserModel.fromMap(value.data());
-          setState(() {});
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.userModel = UserModel.fromMap(value.data());
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // userName = Text(
+    //   "${userModel.displayName}",
+    //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    // );
 
-      userName =  Text("${userModel.displayName}", style: TextStyle (fontSize: 20, fontWeight: FontWeight.bold),);
-      userEmail =  Text ("${userModel.email}", style:  TextStyle(color: Colors.black45, fontWeight: FontWeight.w500),);
-
+    // userEmail = Text(
+    //   "${userModel.email}",
+    //   style: TextStyle(color: Colors.black45, fontWeight: FontWeight.w500),
+    // );
 
     return Scaffold(
       appBar: AppBar(
         title: Text("GCSE Maths Educational App"),
         centerTitle: true,
+        actions: [
+          PopupMenuButton<int>(
+              onSelected: (item) => selectedTab (context,item),
+              itemBuilder: (context) =>
+                  [PopupMenuItem<int>(
+                      value: 0, child: Text("Sign Out"))])
+        ],
       ),
       body: Center(
           child: Padding(
         padding: EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-          // SizedBox(
-          //   height: 200,
-          //   child: Image.asset(''
-          //   , fit: BoxFit.contain
-          //   )),],
-            this.userName,
-            SizedBox(height: 10 ,),
-            this.userEmail,
-            SizedBox(height: 15),
-            ActionChip(label: Text( "Logout", style: TextStyle(color:Colors.black45, fontWeight: FontWeight.bold),),
-                avatar: Icon(Icons.logout, color: Colors.black45,),
-                onPressed: (){
-              logout(context);
-                })
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              // SizedBox(
+              //   height: 200,
+              //   child: Image.asset(''
+              //   , fit: BoxFit.contain
+              //   )),],
+              //this.userName,
+              SizedBox(
+                height: 10,
+              ),
+              //this.userEmail,
+              SizedBox(height: 15),
+              // ActionChip(
+              //     label: Text(
+              //       "Logout",
+              //       style: TextStyle(
+              //           color: Colors.black45, fontWeight: FontWeight.bold),
+              //     ),
+              //     avatar: Icon(
+              //       Icons.logout,
+              //       color: Colors.black45,
+              //     ),
+              //     onPressed: () {
+              //       logout(context);
+              //     })
 
-          ]
-        ),
-      )
-      ),
+
+
+            ]),
+      )),
     );
   }
 
-
-  Future<void> logout (BuildContext context) async {
+  Future<void> logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     await _googleSignIn.signOut();
 
-    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)
-    => LoginScreen()), (route) => false);
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+        (route) => false);
+  }
+
+  void selectedTab (BuildContext context, int item) {
+
+    switch (item) {
+      case 0:
+        logout(context);
+        break;
+      case 1:
+        break ;
+    }
   }
 }
